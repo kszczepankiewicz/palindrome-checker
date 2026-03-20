ee4ea3aac2513e3f8646af30eca91bd49696fcda
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-03-20 16:13:52 +0100

    refactor: rename oldValue to original

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const textInput = document.getElementById('text-input');
 const result = document.getElementById('result');
 
 checkBtn.addEventListener('click', () => {
-    const { value: oldValue } = textInput;
-    const value = oldValue.replaceAll(/[-_ ,\.\(\)\\\/]/g, '').toLowerCase();
+    const { value: original } = textInput;
+    const value = original.replaceAll(/[-_ ,\.\(\)\\\/]/g, '').toLowerCase();
     if (value === '') alert('Please input a value');
-    if (value === [...value].reverse().join('')) result.textContent = `${oldValue}  is a palindrome`;
-    else result.textContent = `${oldValue} is not  a palindrome`
+    if (value === [...value].reverse().join('')) result.textContent = `${original}  is a palindrome`;
+    else result.textContent = `${original} is not  a palindrome`
 });
