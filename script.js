20b994a6e3c2d938e0a5eccb23663fa9aef9008d
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-03-20 09:58:15 +0100

    feat: complete script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+console.clr;
+
+const checkBtn = document.getElementById('check-btn');
+const textInput = document.getElementById('text-input');
+const result = document.getElementById('result');
+
+checkBtn.addEventListener('click', () => {
+    const { value: oldValue } = textInput;
+    const value = oldValue.replaceAll(/[-_ ,\.\(\)\\\/]/g, '').toLowerCase();
+    if (value === '') alert('Please input a value');
+    if (value === [...value].reverse().join('')) result.textContent = `${oldValue}  is a palindrome`;
+    else result.textContent = `${oldValue} is not  a palindrome`
+});
